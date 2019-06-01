139aff788776633f4af6828c5678d3a739a45730
Mathias Fleury <Mathias.Fleury@mpi-inf.mpg.de>
2019-06-01 11:28:56 +0200

    improve indentation

--- src/Tools/emacs-lsp/lsp-isar/lsp-indent.el
+++ src/Tools/emacs-lsp/lsp-isar/lsp-indent.el
@@ -1,187 +1,206 @@
+(require 'cl)
+
 (defun current-line-empty-p ()
   (string-match-p "^\\s-*$" (thing-at-point 'line)))
 
+(defun create-regex-from-words (s)
+  (concat
+   (cl-reduce (lambda (w y) (concat w "\\|" y))
+  (mapcar (lambda (w) (concat "\\(" w "\\)")) s)))
+  )
+
 (setq lsp-isar-keyw-quasi-command
-      (concat
-       "and\\|assumes\\|constrains\\|defines\\|fixes\\|for\\|if\\|includes\\|notes\\|rewrites\\|"
-       "obtains\\|shows\\|when\\|where")) ;; missing "|"
+      (list
+       "and" "assumes" "constrains" "defines" "fixes" "for" "if" "includes" "notes" "rewrites"
+       "obtains" "shows" "when" "where" "imports" "theory")) ;; missing "|"
 
 (setq lsp-isar--keyw-quasi-command
-      (concat "^[ \t]*\\(" lsp-isar-keyw-quasi-command "\\)"))
+      (create-regex-from-words lsp-isar-keyw-quasi-command))
 
 (setq lsp-isar-keyw-command-open
-      "begin\\|proof")
+      (list "begin" "proof"))
 
 (setq lsp-isar--keyw-command-open
-      (concat "^[ \t]*\\(" lsp-isar-keyw-command-open "\\)"))
+      (create-regex-from-words lsp-isar-keyw-command-open))
 
 (setq lsp-isar-keyw-command-close
-      "qed\\|end")
+      (list "qed" "end"))
 
 (setq lsp-isar--keyw-command-close
-      (concat "^[ \t]*\\(" lsp-isar-keyw-command-close "\\)"))
+      (create-regex-from-words lsp-isar-keyw-command-close))
 
 (setq lsp-isar-keyw-theory
-      "theory\\|imports\\|end\\|begin")
+      (list "theory" "imports" "end" "begin"))
 
 (setq lsp-isar--keyw-theory
-      (concat "^[ \t]*\\(" lsp-isar-keyw-theory "\\)"))
+      (create-regex-from-words lsp-isar-keyw-theory))
 
 (setq lsp-isar-keyw-diag
-      (concat
-       "export_generated_files\\|ML_val\\|ML_command\\|print_bundles\\|help\\|"
-       "print_commands\\|print_options\\|print_context\\|print_theory\\|print_definitions\\|"
-       "print_syntax\\|print_abbrevs\\|print_defn_rules\\|print_theorems\\|print_locales\\|"
-       "print_classes\\|print_locale\\|print_interps\\|print_dependencies\\|print_attributes\\|"
-       "print_simpset\\|print_rules\\|print_trans_rules\\|print_methods\\|"
-       "print_antiquotations\\|print_ML_antiquotations\\|thy_deps\\|locale_deps\\|class_deps\\|"
-       "thm_deps\\|print_term_bindings\\|print_facts\\|print_cases\\|print_statement\\|thm\\|prf\\|"
-       "full_prf\\|prop\\|term\\|typ\\|print_codesetup\\|unused_thms\\|print_state\\|welcome\\|"
-       "find_theorems\\|find_consts"))
+      (list
+       "export_generated_files" "ML_val" "ML_command" "print_bundles" "help"
+       "print_commands" "print_options" "print_context" "print_theory" "print_definitions"
+       "print_syntax" "print_abbrevs" "print_defn_rules" "print_theorems" "print_locales"
+       "print_classes" "print_locale" "print_interps" "print_dependencies" "print_attributes"
+       "print_simpset" "print_rules" "print_trans_rules" "print_methods"
+       "print_antiquotations" "print_ML_antiquotations" "thy_deps" "locale_deps" "class_deps"
+       "thm_deps" "print_term_bindings" "print_facts" "print_cases" "print_statement" "thm" "prf"
+       "full_prf" "prop" "term" "typ" "print_codesetup" "unused_thms" "print_state" "welcome"
+       "find_theorems" "find_consts"))
 
 (setq lsp-isar--keyw-diag
-      (concat "^[ \t]*\\(" lsp-isar-keyw-diag "\\)"))
+      (create-regex-from-words lsp-isar-keyw-diag))
 
 (setq lsp-isar-keyw-thy_decl
-      (concat
-       "default_sorts\\|typedecl\\|type_synonym\\|nonterminal\\|judgement\\|"
-       "typedecl\\|" "type_synonym\\|" "nonterminal\\|" "judgment\\|"
-       "consts\\|" "syntax\\|" "no_syntax\\|" "translations\\|" "no_translations\\|"
-       "definition\\|" "abbreviation\\|" "type_notation\\|" "no_type_notation\\|" "notation\\|"
-       "no_notation\\|" "axiomatization\\|" "alias\\|" "type_alias\\|" "lemmas\\|" "declare\\|"
-       "hide_class\\|" "hide_type\\|" "hide_const\\|" "hide_fact"))
+      (list
+       "default_sorts" "typedecl" "type_synonym" "nonterminal" "judgement"
+       "typedecl" "type_synonym" "nonterminal" "judgment"
+       "consts" "syntax" "no_syntax" "translations" "no_translations"
+       "definition" "abbreviation" "type_notation" "no_type_notation" "notation"
+       "no_notation" "axiomatization" "alias" "type_alias" "lemmas" "declare"
+       "hide_class" "hide_type" "hide_const" "hide_fact"))
 
 (setq lsp-isar--keyw-thy_decl
-      (concat "^[ \t]*\\(" lsp-isar-keyw-thy_decl "\\)"))
+      (create-regex-from-words lsp-isar-keyw-thy_decl))
 
 (setq lsp-isar-keyw-thy-goal
-      (concat "theorem\\|" "lemma\\|" "corollary\\|" "proposition"))
+      (list "theorem" "lemma" "corollary" "proposition"))
 
 (setq lsp-isar--keyw-thy-goal
-      (concat "^[ \t]*\\(" lsp-isar-keyw-thy-goal "\\)"))
+      (create-regex-from-words lsp-isar-keyw-thy-goal))
 
 (setq lsp-isar-thy_decl_block
-      (concat "context\\|" "locale\\|" "experiment\\|"
-	      "bundle\\|" "instantiation\\|" "overloading\\|"
+      (list "context" "locale" "experiment"
+	      "bundle" "instantiation" "overloading"
 	      "notepad"))
 
 (setq lsp-isar--keyw-thy_decl
-      (concat "^[ \t]*\\(" lsp-isar-keyw-thy_decl "\\)"))
+      (create-regex-from-words lsp-isar-keyw-thy_decl))
 
 (setq indent_offset 2)
 
+(setq lsp-isar-keyw-qed
+      (list "by" "\\.\\." "\\." "sorry" "\\\\<proof>"))
+
+(setq lsp-isar--keyw-qed
+      (create-regex-from-words lsp-isar-keyw-qed))
+
 (setq lsp-isar-keyw-proof-enclose
-      "qed\\|done\\|end\\|next")
+      (list "qed" "done" "end" "next"))
 
 (setq lsp-isar--keyw-proof-enclose
-      (concat "^[ \t]*\\(" lsp-isar-keyw-proof-enclose "\\)"))
+      (create-regex-from-words lsp-isar-keyw-proof-enclose))
 
 (setq lsp-isar-keyw-open-bracket
-      "{")
+      (list "{"))
 
 (setq lsp-isar--keyw-open-bracket
-      (concat "^[ \t]*\\(" lsp-isar-keyw-open-bracket "\\)"))
+      (create-regex-from-words lsp-isar-keyw-open-bracket))
 
 (setq lsp-isar-keyw-close-bracket
-      "{")
+      (list "}"))
 
 (setq lsp-isar--keyw-close-bracket
-      (concat "^[ \t]*\\(" lsp-isar-keyw-close-bracket "\\)"))
+      (create-regex-from-words lsp-isar-keyw-close-bracket))
 
 
 (setq lsp-isar-keyw-begin
-      (concat "begin"))
+      (list "begin"))
 
 (setq lsp-isar--keyw-begin
-      (concat "^[ \t]*\\(" lsp-isar-keyw-begin "\\)"))
+      (create-regex-from-words lsp-isar-keyw-begin))
 
-(setq lsp-isar-keyw-before
-      (concat "private\\|qualified"))
+(setq lsp-isar-keyw-before-command
+      (list "private" "qualified"))
 
-(setq lsp-isar--keyw-before
-      (concat "^[ \t]*\\(" lsp-isar-keyw-before "\\)"))
+(setq lsp-isar--keyw-before-command
+      (create-regex-from-words lsp-isar-keyw-before-command))
 
 
 (setq lsp-isar-keyw-proof-script
-      (concat "supply\\|guess\\|defer\\|prefer\\|apply\\|"
-	      "apply_end\\|subgoal\\|back"))
+      (list "supply" "guess" "defer" "prefer" "apply"
+	    "apply_end" "subgoal" "back"
+	    ;;hack
+	    "by" "sorry"))
 
 (setq lsp-isar--keyw-proof-script
-      (concat "^[ \t]*\\(" lsp-isar-keyw-proof-script "\\)"))
+      (create-regex-from-words lsp-isar-keyw-proof-script))
 
 (setq lsp-isar-keyw-prf_asm
-      (concat "fix\\|assume\\|presume\\|define\\|case"))
+      (list "fix" "assume" "presume" "define" "case"))
 
 (setq lsp-isar--keyw-prf_asm
-      (concat "^[ \t]*\\(" lsp-isar-keyw-prf_asm "\\)"))
+      (create-regex-from-words lsp-isar-keyw-prf_asm))
 
 (setq lsp-isar-keyw-prf_asm_goal
-      (concat "obtain\\|show\\|thus"))
+      (list "obtain" "show" "thus"))
 
 (setq lsp-isar--keyw-prf_asm_goal
-      (concat "^[ \t]*\\(" lsp-isar-keyw-prf_asm_goal "\\)"))
+      (create-regex-from-words lsp-isar-keyw-prf_asm_goal))
 
 (setq lsp-isar-keyw-prf_goal
-      (concat "have\\|hence\\|consider"))
+      (list "have" "hence" "consider"))
 
 (setq lsp-isar--keyw-prf_goal
-      (concat "^[ \t]*\\(" lsp-isar-keyw-prf_goal "\\)"))
+      (create-regex-from-words lsp-isar-keyw-prf_goal))
 
 
 (setq lsp-isar-keyw-prf_decl
-      (concat "using\\|unfolding"))
+      (list "using" "unfolding"))
 
-(setq lsp-isar--keyw-prf_goal
-      (concat "^[ \t]*\\(" lsp-isar-keyw-prf_decl "\\)"))
+(setq lsp-isar--keyw-prf_decl
+      (create-regex-from-words lsp-isar-keyw-prf_decl))
 
 (setq lsp-isar-keyw-proof
-      "qed\\|done\\|end\\|next\\|oops\\|proof")
+      (append (list "qed" "done" "end" "next" "oops" "proof") lsp-isar-keyw-qed
+	      ))
 
 (setq lsp-isar--keyw-proof
-      (concat "^[ \t]*\\("
-	      lsp-isar-keyw-proof
-	      "\\)"))
+      (create-regex-from-words lsp-isar-keyw-proof))
 
-(setq lsp-isar-keyw-qed
-      "by\\|..\\|.\\|sorry\\|\\\\<proof>")
+;; HACK
+(setq lsp-isar--keyw-proof-hack
+      (create-regex-from-words lsp-isar-keyw-qed))
 
-(setq lsp-isar--keyw-qed
-      (concat "^[ \t]*\\("
-	      lsp-isar-keyw-qed
-	      "\\)"))
+(setq lsp-isar-keyw-HOL-command
+      (list "export_code"))
 
+(setq lsp-isar--keyw-HOL-command
+      (create-regex-from-words lsp-isar-keyw-HOL-command))
 
 (setq lsp-isar-keyw-command
-      (concat lsp-isar-keyw-proof "\\|"
-	      lsp-isar-keyw-quasi-command "\\|"
-	      lsp-isar-keyw-thy_decl "\\|"
-	      lsp-isar-keyw-diag "\\|"
-	      lsp-isar-keyw-thy-goal "\\|"
-	      lsp-isar-thy_decl_block "\\|"
-	      lsp-isar-keyw-prf_asm "\\|"
-	      lsp-isar-keyw-prf_asm_goal "\\|"
-	      lsp-isar-keyw-prf_decl "\\|"
-	      lsp-isar-keyw-prf_goal))
+      (append lsp-isar-keyw-proof
+	      lsp-isar-keyw-quasi-command
+	      lsp-isar-keyw-thy_decl
+	      lsp-isar-keyw-diag
+	      lsp-isar-keyw-thy-goal
+	      lsp-isar-thy_decl_block
+	      lsp-isar-keyw-prf_asm
+	      lsp-isar-keyw-prf_asm_goal
+	      lsp-isar-keyw-prf_goal
+	      lsp-isar-keyw-prf_decl
+	      lsp-isar-keyw-HOL-command
+	      ))
 
 (setq lsp-isar-keyw-begin-or-command
-      (concat "begin\\|"
-	       lsp-isar-keyw-command))
+      (cons "begin" lsp-isar-keyw-command))
 
 (setq lsp-isar--keyw-begin-or-command
-      (concat "^[ \t]*\\("
-	      lsp-isar-keyw-begin-or-command
-	      "\\)"))
+      (create-regex-from-words lsp-isar-keyw-begin-or-command))
 
 (setq lsp-isar--keyw-command
-      (concat "^[ \t]*\\("
-	      lsp-isar-keyw-begin-or-command
-	      "\\)"))
+      (create-regex-from-words lsp-isar-keyw-command))
 
 ;; looking-at-p can match the next line...
 (defun looking-at-p-nonempty (a)
   (and
    (/= 0 (current-line-empty-p))
-   (looking-at-p a)))
+   (word-at-point)
+   (string-match-p a (word-at-point))))
+
+(defun move-to-first-word-on-the-line ()
+  (message "move-to-first-word-on-the-line %s" (word-at-point))
+  (if (and (not (word-at-point)) (not (looking-at "[[:space:]]*$")))
+      (right-word)))
 
 (defun indent_indent ()
   (if (looking-at-p-nonempty lsp-isar--keyw-command-open)
@@ -191,15 +210,22 @@
       0)))
 
 (defun indent_offset ()
-  (if (looking-at-p-nonempty lsp-isar--keyw-command)
+  (move-to-first-word-on-the-line)
+  (if (looking-at-p-nonempty lsp-isar--keyw-proof-enclose)
       indent_offset
+    0))
+
+(defun script_indent ()
+  (move-to-first-word-on-the-line)
+  (if (looking-at-p-nonempty lsp-isar--keyw-proof-hack)
+      (+ indent_offset)
     0))
 
 (defun indent_brackets ()
   (message "indent_brackets")
   (setq finished nil)
-  (setq depth 0)
   (save-excursion
+    (setq depth 0)
     (while (and (not finished) (not (= (point) (point-min))))
       (if (looking-at-p-nonempty lsp-isar--keyw-begin-or-command)
 	  (setq finished t))
@@ -207,7 +233,8 @@
 	  (setq depth (+ depth indent_offset)))
       (if (looking-at-p-nonempty lsp-isar--keyw-close-bracket)
 	  (setq depth (- depth indent_offset)))
-      (forward-line -1)))
+      (forward-line -1)
+      ))
   depth)
 
 (defun indent_extra ()
@@ -227,64 +254,77 @@
   (message "indent_structure")
   (interactive)
   (setq finished nil)
-  (setq depth 0)
   ;; TODO only for debugging
-  (beginning-of-line)
   (save-excursion
+    (beginning-of-line)
+    (forward-line -1)
+    (setq depth 0)
     (while (and (not finished) (not (= (point) (point-min))))
-      (message "indent_structure %s %s, indent at: %s" (word-at-point) depth (indent_indent))
-      (setq depth (+ depth (indent_indent)))
-      (if (and
-	   (looking-at-p-nonempty lsp-isar--keyw-begin-or-command)
-	   (not (looking-at-p-nonempty lsp-isar--keyw-proof-script)))
-	  (progn
-	    (setq finished t)
-	    (setq depth (+ depth (indent_offset)
-			   ;;(current-indentation)
-			   ))))
-      (forward-line -1)))
-  (message "indent_structure %s" depth)
-  depth)
+      (beginning-of-line)
+      (if (looking-at "[[:space:]]*$")
+	  (forward-line -1)
+	(progn
+	  (move-to-first-word-on-the-line)
+	  (message "indent_structure %s %s, indent at: %s" (word-at-point) depth (indent_indent))
+	  (setq depth (+ depth (indent_indent)))
+          (message "indent_structure %s" depth)
+	  (if (and
+	       (looking-at-p-nonempty lsp-isar--keyw-begin-or-command)
+	       (not (looking-at-p-nonempty lsp-isar--keyw-proof-script)))
+	      (progn
+		(setq finished t)
+                (message "indent_structure %s with %s: %s " depth (indent_offset) (+ depth (indent_offset)))
+		(setq depth (+ depth (current-indentation) (indent_offset)))
+		(message "indent_structure set Top %s" depth))
+	      (forward-line -1)))))
+    (message "indent_structure finl: %s" depth)
+    depth))
 
 (defun goto-prev-line-command ()
   (message "goto-prev-line-command")
   (setq finished nil)
   (while (and (not finished) (not (= (point) (point-min))))
-    (message "%s" (thing-at-point 'line))
+    (move-to-first-word-on-the-line)
+    (message "goto-prev-line-command: %s" (word-at-point))
     (if (looking-at-p-nonempty lsp-isar--keyw-begin-or-command)
-	(setq finished t))
-    (forward-line -1))
+	(setq finished t)
+      (forward-line -1)))
   depth)
 
 (defun indentation_depth (indent_structure indent_brackets)
   (message "indentation_depth")
   (save-excursion
-    (if (looking-at-p-nonempty lsp-isar--keyw-begin-or-command)
+    (move-to-first-word-on-the-line)
+    (message "indentation_depth, init word: %s, command? %s" (word-at-point)
+	     (string-match-p lsp-isar--keyw-begin-or-command (word-at-point)))
+    (if (/= 0 (current-line-empty-p))
 	(if (or
 	     (looking-at-p-nonempty lsp-isar--keyw-begin)
-	     (looking-at-p-nonempty lsp-isar--keyw-before)
+	     (looking-at-p-nonempty lsp-isar--keyw-before-command)
 	     (looking-at-p-nonempty lsp-isar--keyw-theory))
 	    0
-	  (if (looking-at-p-nonempty lsp-isar--keyw-proof-enclose)
+	  (if (or (looking-at-p-nonempty lsp-isar--keyw-proof-enclose))
               (progn
 		(message "command enclose")
 		(- indent_structure indent_offset))
 	    (if (looking-at-p-nonempty lsp-isar--keyw-proof)
 		(progn
-		  (message "command proof")
-		  (max (- indent_structure indent_offset) indent_offset))
+		  (message "command proof %s %s %s"  indent_structure (script_indent) (indent_offset))
+		  (max (+ indent_structure (script_indent) (- (indent_offset))) 0))
 	      (progn
-		(if (and nil
-			 (looking-at-p-nonempty lsp-isar--keyw-command))
+		(if (looking-at-p-nonempty lsp-isar--keyw-command)
 		    (progn
 		      (message "command")
-		      (- indent_structure indent_offset))
+		      (+ indent_structure (- (indent_offset))))
 		  (if (looking-at-p-nonempty lsp-isar--keyw-proof-enclose)
-		      (max (- indent_structure indent_offset) indent_offset)
+		      (progn
+			(message "lsp-isar--keyw-proof-enclose")
+			(max (- indent_structure indent_offset) indent_offset))
 		    (progn
 		      (message "command+no enclose")
 		      (let ((curr_quasi (looking-at-p-nonempty lsp-isar--keyw-quasi-command)))
-			(forward-line -1)
+		        (goto-prev-line-command)
+			(move-to-first-word-on-the-line)
 			(let* ((prev_quasi (looking-at-p-nonempty lsp-isar--keyw-quasi-command))
 			       (extra
 				(if (or (and curr_quasi prev_quasi)
@@ -292,34 +332,39 @@
 				    0
 				  (if (or (and curr_quasi (not prev_quasi)))
 				      (- (indent_extra))
-				    ((indent_extra))))))
-			  (message "%s" extra)
-			  (+ indent_structure indent_brackets extra (- indent_offset)))))))))))
+				    (indent_extra)))))
+			  (message "extra %s %s %s" extra prev_quasi curr_quasi)
+			  (+ indent_structure indent_brackets extra (- (indent_offset))))))))))))
       ;; no command at the head
       (progn
 	(message "indentation_depth/no command")
+	(message "word at point: %s" (word-at-point))
 	(goto-prev-line-command)
 	(message "word at point: %s" (word-at-point))
-	(message "indent-indent: %s; (indent_offset): %s; command: %s"  (indent_indent)
+	(message "indent-indent: %s; indent_offset: %s; command: %s"  (indent_indent)
 		 (indent_offset) (looking-at-p-nonempty lsp-isar--keyw-command))
 	(if (looking-at-p-nonempty lsp-isar--keyw-command)
 	    (+ indent_structure indent_brackets indent_offset
 	       (- (indent_indent)) (- (indent_offset)))
 	  (let ((extra (if (looking-at-p-nonempty lsp-isar--keyw-quasi-command) (indent_extra) 0)))
+	    (message "indent_structure: %s; indent_brackets: %s; extra: %s; indent_offset: %s"
+		     indent_structure indent_brackets extra indent_offset)
 	    (+ indent_structure indent_brackets extra indent_offset)))))))
 
 (defun isar-indent-line ()
   "Indent current line as Isar code"
   (interactive)
   (beginning-of-line)
+  (message "************************")
   (let* (
 	 (cur-struct-indent (indent_structure))
 	 (cur-bracket-indent (indent_brackets))
-	 (cur-indent (indentation_depth cur-struct-indent cur-bracket-indent )))
+	 (cur-indent (max 0 (indentation_depth cur-struct-indent cur-bracket-indent ))))
     ;;(if cur-indent
     ;;(indent-line-to cur-indent)
     (message "cur-struct-indent: %s" cur-struct-indent)
     (message "cur-bracket-indent: %s" cur-bracket-indent)
-    (message "cur-indent: %s" cur-indent))
-  t)
-;;(indent-line-to 0))))
+    (message "cur-indent: %s" cur-indent)
+    (indent-line-to cur-indent)))
+
+; (set (make-local-variable 'indent-line-function) 'isar-indent-line)
