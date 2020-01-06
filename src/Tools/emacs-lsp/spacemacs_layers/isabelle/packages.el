14b678312cc400095bb2d502e223d71372ee643d
Mathias Fleury <Mathias.Fleury@mpi-inf.mpg.de>
2020-01-06 09:13:19 +0100

    tune spacemacs configuration

--- src/Tools/emacs-lsp/spacemacs_layers/isabelle/packages.el
+++ src/Tools/emacs-lsp/spacemacs_layers/isabelle/packages.el
@@ -1,11 +1,9 @@
 ;;; packages.el --- isabelle layer packages file for Spacemacs.
 ;;
-;; Copyright (c) 2012-2017 Sylvain Benner & Contributors
+;; Copyright (c) 2018-2020 Mathias Fleury
 ;;
 ;; Author: Mathias Fleury, Sophie
-;; URL: https://github.com/syl20bnr/spacemacs
-;;
-;; This file is not part of GNU Emacs.
+;; URL: https://github.com/syl20bnr/spacemacshttps://bitbucket.org/zmaths/isabelle2019-vsce/
 ;;
 ;;; License: GPLv3
 
@@ -27,58 +25,24 @@
 ;;   define the functions `isabelle/pre-init-PACKAGE' and/or
 ;;   `isabelle/post-init-PACKAGE' to customize the package as it is loaded.
 
+;; async and yasnippet are already included somewhere else
+
 ;;; Code:
 
 (defconst isabelle-packages
   '(
+    ;; async is in spacemacs-core
     dash
-    async
-    yasnippet
     (isar-mode :location local)
     (isar-goal-mode :location local)
-    (lsp-isar :location local))
-  "The list of Lisp packages required by the isabelle layer.
-
-Each entry is either:
-
-1. A symbol, which is interpreted as a package to be installed, or
-
-2. A list of the form (PACKAGE KEYS...), where PACKAGE is the
-    name of the package to be installed or loaded, and KEYS are
-    any number of keyword-value-pairs.
-
-    The following keys are accepted:
-
-    - :excluded (t or nil): Prevent the package from being loaded
-      if value is non-nil
-
-    - :location: Specify a custom installation location.
-      The following values are legal:
-
-      - The symbol `elpa' (default) means PACKAGE will be
-        installed using the Emacs package manager.
-
-      - The symbol `local' directs Spacemacs to load the file at
-        `./local/PACKAGE/PACKAGE.el'
-
-      - A list beginning with the symbol `recipe' is a melpa
-        recipe.  See: https://github.com/milkypostman/melpa#recipe-format")
+    (lsp-isar :location local)
+    yasnippet)
+  "The list of Lisp packages required by the isabelle layer.")
 
 
 (defun isabelle/init-dash ()
   (use-package dash
     :defer t))
-
-
-(defun isabelle/init-async ()
-  (use-package async
-    :defer t))
-
-(defun isabelle/init-yasnippet ()
-  (use-package yasnippet))
-
-(defun isabelle/post-init-yasnippet ()
-  (push (concat lsp-isar-path-to-isabelle "/src/Tools/emacs-lsp/yasnippet") yas-snippet-dirs))
 
 (defun isabelle/init-isar-mode ()
   (use-package isar-mode
@@ -87,6 +51,9 @@ Each entry is either:
     :after isar-mode))
 
 
+(defun isabelle/post-init-yasnippet ()
+  (push (concat lsp-isar-path-to-isabelle "/src/Tools/emacs-lsp/yasnippet") yas-snippet-dirs))
+
 (defun isabelle/init-isar-goal-mode ()
   (use-package isar-goal-mode
     :after isar-mode))
@@ -94,18 +61,13 @@ Each entry is either:
 (defun isabelle/init-lsp-isar ()
   (use-package lsp-isar
     :after isar-mode isar-goal-mode
-    ;:hook
-    ;((isar-mode-hook . lsp-isar-enable))
     :config
     (setq lsp-response-timeout 1200)
     (setq lsp-restart 'ignore)
-    (setq lsp-prefer-flymake nil)
-    )
+    (setq lsp-prefer-flymake nil))
   (add-hook 'isar-mode-hook 'flycheck-mode)
   (add-hook 'isar-mode-hook 'lsp-isar-define-client-and-start)
-
-  (add-hook 'lsp-isar-init-hook 'lsp-isar-open-output-and-progress-right-spacemacs)
- )
+  (add-hook 'lsp-isar-init-hook 'lsp-isar-open-output-and-progress-right-spacemacs))
 
 
 ;;; packages.el ends here
