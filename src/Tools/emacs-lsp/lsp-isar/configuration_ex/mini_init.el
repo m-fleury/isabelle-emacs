e29452ff674e2cd1f6467ba1949dd2401657e90f
Mathias Fleury <Mathias.Fleury@ens-rennes.fr>
2022-10-27 18:11:18 +0200

    import vsce

---
+++ src/Tools/emacs-lsp/lsp-isar/configuration_ex/mini_init.el
@@ -0,0 +1,68 @@
+;; initialisation of package
+(require 'package)
+(package-initialize)
+
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+
+(unless (package-installed-p 'use-package)
+  (message "installing package use-package")
+  (package-refresh-contents)
+  (package-install 'use-package)
+
+  (unless (package-installed-p 'use-package)
+    (error "failed to install use-package"))
+  )
+
+;; install quelpa
+(use-package quelpa
+  :ensure t)
+
+(quelpa
+ '(quelpa-use-package
+   :fetcher git
+   :url "https://github.com/quelpa/quelpa-use-package.git"))
+
+(require 'quelpa-use-package)
+
+
+(use-package lsp-mode
+  :ensure t)
+
+(use-package async
+  :ensure t)
+
+(use-package isar-mode
+  :ensure t
+  :mode "\\.thy\\'"
+  :quelpa (isar-mode :fetcher github
+		     :repo "m-fleury/isar-mode"
+		     :files ("*.el")
+		     :upgrade t)
+  )
+
+(use-package isar-goal-mode
+  :ensure t
+  :quelpa (isar-goal-mode :fetcher github
+			  :repo "m-fleury/isar-mode"
+			  :files ("*.el")
+			  :upgrade t))
+
+(use-package lsp-isar
+	     :ensure t
+	     :quelpa ((lsp-isar :fetcher github
+				:repo "m-fleury/isabelle-release"
+				:branch "Isabelle2020-more-vscode"
+				:files ("src/Tools/emacs-lsp/lsp-isar/*.el"))
+				:upgrade t)
+  :after lsp-mode
+  :commands lsp-isar-define-client-and-start
+  :defer t
+  :init
+  (add-hook 'isar-mode-hook #'lsp-isar-define-client-and-start)
+  (add-hook 'lsp-isar-init-hook 'lsp-isar-open-output-and-progress-right-spacemacs)
+  :config
+
+  ;; CHANGE HERE: path to isabelle-release repo
+  (setq lsp-isar-path-to-isabelle "~/Documents/isabelle/isabelle-release")
+
+  )
