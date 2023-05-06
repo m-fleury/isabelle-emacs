59519c15df9299d7ba8c23b3c8faf8741fceb035
Hanna Lachnitt <lachnitt@stanford.edu>
2023-05-05 22:59:28 -0700

    new boolean rewrites

--- src/HOL/Tools/SMT/cvc5_dsl_rewrites/Boolean_Rewrites.thy
+++ src/HOL/Tools/SMT/cvc5_dsl_rewrites/Boolean_Rewrites.thy
@@ -15,72 +15,72 @@ re_concat,
 str_concat,
 *)
 
-named_theorems rewrite_bool_double_not_elim \<open>automatically_generated\<close>
+named_theorems bool_double_not_elim \<open>automatically_generated\<close>
 
-lemma [rewrite_bool_double_not_elim]:
+lemma [bool_double_not_elim]:
   fixes t::"bool"
   shows "(\<not> \<not> t) = t"
   by auto
 
-named_theorems rewrite_bool_eq_true \<open>automatically_generated\<close>
+named_theorems bool_eq_true \<open>automatically_generated\<close>
 
-lemma [rewrite_bool_eq_true]:
+lemma [bool_eq_true]:
   fixes t::"bool"
   shows "(t = True) = t"
   by auto
 
-named_theorems rewrite_bool_eq_false \<open>automatically_generated\<close>
+named_theorems bool_eq_false \<open>automatically_generated\<close>
 
-lemma [rewrite_bool_eq_false]:
+lemma [bool_eq_false]:
   fixes t::"bool"
   shows "(t = False) = (\<not> t)"
   by auto
 
-named_theorems rewrite_bool_eq_nrefl \<open>automatically_generated\<close>
+named_theorems bool_eq_nrefl \<open>automatically_generated\<close>
 
-lemma [rewrite_bool_eq_nrefl]:
+lemma [bool_eq_nrefl]:
   fixes x::"bool"
   shows "(x = (\<not> x)) = False"
   by auto
 
-named_theorems rewrite_bool_impl_false1 \<open>automatically_generated\<close>
+named_theorems bool_impl_false1 \<open>automatically_generated\<close>
 
-lemma [rewrite_bool_impl_false1]:
+lemma [bool_impl_false1]:
   fixes t::"bool"
   shows "(t \<longrightarrow> False) = (\<not> t)"
   by auto
 
-named_theorems rewrite_bool_impl_false2 \<open>automatically_generated\<close>
+named_theorems bool_impl_false2 \<open>automatically_generated\<close>
 
-lemma [rewrite_bool_impl_false2]:
+lemma [bool_impl_false2]:
   fixes t::"bool"
   shows "(False \<longrightarrow> t) = True"
   by auto
 
-named_theorems rewrite_bool_impl_true1 \<open>automatically_generated\<close>
+named_theorems bool_impl_true1 \<open>automatically_generated\<close>
 
-lemma [rewrite_bool_impl_true1]:
+lemma [bool_impl_true1]:
   fixes t::"bool"
   shows "(t \<longrightarrow> True) = True"
   by auto
 
-named_theorems rewrite_bool_impl_true2 \<open>automatically_generated\<close>
+named_theorems bool_impl_true2 \<open>automatically_generated\<close>
 
-lemma [rewrite_bool_impl_true2]:
+lemma [bool_impl_true2]:
   fixes t::"bool"
   shows "(True \<longrightarrow> t) = t"
   by auto
 
-named_theorems rewrite_bool_impl_elim \<open>automatically_generated\<close>
+named_theorems bool_impl_elim \<open>automatically_generated\<close>
 
-lemma [rewrite_bool_impl_elim]:
+lemma [bool_impl_elim]:
   fixes t::"bool" and s::"bool"
   shows "(t \<longrightarrow> s) = (\<not> t \<or> s)"
   by auto
 
-named_theorems rewrite_bool_or_true \<open>automatically_generated\<close>
+named_theorems bool_or_true \<open>automatically_generated\<close>
 
-lemma [rewrite_bool_or_true]:
+lemma [bool_or_true]:
   fixes xs::"bool cvc_ListVar" and ys::"bool cvc_ListVar"
   shows "cvc_list_left (\<or>) xs (cvc_list_right (\<or>) True ys) = True"
   apply (cases ys)
@@ -89,9 +89,9 @@ lemma [rewrite_bool_or_true]:
     by (simp add: cvc_list_left_transfer cvc_list_right_transfer_op cvc_list_both_transfer_op)
   done
 
-named_theorems rewrite_bool_or_false \<open>automatically_generated\<close>
+named_theorems bool_or_false \<open>automatically_generated\<close>
 
-lemma [rewrite_bool_or_false]:
+lemma [bool_or_false]:
   fixes xs::"bool cvc_ListVar" and ys::"bool cvc_ListVar"
   shows "cvc_list_left (\<or>) xs (cvc_list_right (\<or>) False ys) =
    cvc_list_both (\<or>) False xs ys"
@@ -101,9 +101,9 @@ lemma [rewrite_bool_or_false]:
     by (simp add: cvc_list_left_transfer cvc_list_right_transfer_op cvc_list_both_transfer_op)
   done
 
-named_theorems rewrite_bool_or_flatten \<open>automatically_generated\<close>
+named_theorems bool_or_flatten \<open>automatically_generated\<close>
 
-lemma [rewrite_bool_or_flatten]:
+lemma [bool_or_flatten]:
   fixes xs::"bool cvc_ListVar" and b::"bool" and ys::"bool cvc_ListVar" and zs::"bool cvc_ListVar"
   shows "cvc_list_left (\<or>) xs
     (cvc_list_right (\<or>) (cvc_list_right (\<or>) b ys) zs) =
@@ -116,9 +116,9 @@ lemma [rewrite_bool_or_flatten]:
     by (metis foldr_or_neutral)
   done
 
-named_theorems rewrite_bool_or_dup \<open>automatically_generated\<close>
+named_theorems bool_or_dup \<open>automatically_generated\<close>
 
-lemma [rewrite_bool_or_dup]:
+lemma [bool_or_dup]:
   fixes xs::"bool cvc_ListVar" and b::"bool" and ys::"bool cvc_ListVar" and zs::"bool cvc_ListVar"
   shows "cvc_list_left (\<or>) xs
     (b \<or> cvc_list_left (\<or>) ys (cvc_list_right (\<or>) b zs)) =
@@ -131,9 +131,9 @@ lemma [rewrite_bool_or_dup]:
     by (simp add: bool_or_dup_lemma)
   done
 
-named_theorems rewrite_bool_and_true \<open>automatically_generated\<close>
+named_theorems bool_and_true \<open>automatically_generated\<close>
 
-lemma [rewrite_bool_and_true]:
+lemma [bool_and_true]:
   fixes xs::"bool cvc_ListVar" and ys::"bool cvc_ListVar"
   shows "cvc_list_left (\<and>) xs (cvc_list_right (\<and>) True ys) =
          cvc_list_both (\<and>) True xs ys"
@@ -143,9 +143,9 @@ lemma [rewrite_bool_and_true]:
     by (simp add: cvc_list_left_transfer cvc_list_right_transfer_op cvc_list_both_transfer_op)
   done
 
-named_theorems rewrite_bool_and_false \<open>automatically_generated\<close>
+named_theorems bool_and_false \<open>automatically_generated\<close>
 
-lemma [rewrite_bool_and_false]:
+lemma [bool_and_false]:
   fixes xs::"bool cvc_ListVar" and ys::"bool cvc_ListVar"
   shows "cvc_list_left (\<and>) xs (cvc_list_right (\<and>) False ys) = False"
   apply (cases ys)
@@ -154,9 +154,9 @@ lemma [rewrite_bool_and_false]:
     by (simp add: cvc_list_left_transfer cvc_list_right_transfer_op cvc_list_both_transfer_op)
   done
 
-named_theorems rewrite_bool_and_flatten \<open>automatically_generated\<close>
+named_theorems bool_and_flatten \<open>automatically_generated\<close>
 
-lemma [rewrite_bool_and_flatten]:
+lemma [bool_and_flatten]:
   fixes xs::"bool cvc_ListVar" and b::"bool" and ys::"bool cvc_ListVar" and zs::"bool cvc_ListVar"
   shows "cvc_list_left (\<and>) xs
     (cvc_list_right (\<and>) (cvc_list_right (\<and>) b ys) zs) =
@@ -169,9 +169,9 @@ lemma [rewrite_bool_and_flatten]:
     by (metis foldr_and_neutral)
   done
 
-named_theorems rewrite_bool_and_dup \<open>automatically_generated\<close>
+named_theorems bool_and_dup \<open>automatically_generated\<close>
 
-lemma [rewrite_bool_and_dup]:
+lemma [bool_and_dup]:
   fixes xs::"bool cvc_ListVar" and b::"bool" and ys::"bool cvc_ListVar" and zs::"bool cvc_ListVar"
   shows "cvc_list_left (\<and>) xs
     (b \<and> cvc_list_left (\<and>) ys (cvc_list_right (\<and>) b zs)) =
@@ -184,9 +184,9 @@ lemma [rewrite_bool_and_dup]:
     by (simp add: bool_and_dup_lemma)
   done
 
-named_theorems rewrite_bool_and_conf \<open>automatically_generated\<close>
+named_theorems bool_and_conf \<open>automatically_generated\<close>
 
-lemma [rewrite_bool_and_conf]:
+lemma [bool_and_conf]:
   fixes xs::"bool cvc_ListVar" and w::"bool" and ys::"bool cvc_ListVar" and zs::"bool cvc_ListVar"
   shows "cvc_list_left (\<and>) xs
     (w \<and>
@@ -200,9 +200,9 @@ lemma [rewrite_bool_and_conf]:
     by (simp add: bool_and_conf_lemma)
   done
 
-named_theorems rewrite_bool_or_taut \<open>automatically_generated\<close>
+named_theorems bool_or_taut \<open>automatically_generated\<close>
 
-lemma [rewrite_bool_or_taut]:
+lemma [bool_or_taut]:
   fixes xs::"bool cvc_ListVar" and w::"bool" and ys::"bool cvc_ListVar" and zs::"bool cvc_ListVar"
   shows "cvc_list_left (\<or>) xs
     (w \<or>
@@ -216,93 +216,93 @@ lemma [rewrite_bool_or_taut]:
     by (simp add: bool_or_taut_lemma)
   done
 
-named_theorems rewrite_bool_xor_refl \<open>automatically_generated\<close>
+named_theorems bool_xor_refl \<open>automatically_generated\<close>
 
-lemma [rewrite_bool_xor_refl]:
+lemma [bool_xor_refl]:
   fixes x::"bool"
   shows "x [+] x = False"
   using xor_def by auto
 
-named_theorems rewrite_bool_xor_nrefl \<open>automatically_generated\<close>
+named_theorems bool_xor_nrefl \<open>automatically_generated\<close>
 
-lemma [rewrite_bool_xor_nrefl]:
+lemma [bool_xor_nrefl]:
   fixes x::"bool"
   shows "x [+] (\<not> x) = True"
   using xor_def by auto
 
-named_theorems rewrite_bool_xor_false \<open>automatically_generated\<close>
+named_theorems bool_xor_false \<open>automatically_generated\<close>
 
-lemma [rewrite_bool_xor_false]:
+lemma [bool_xor_false]:
   fixes x::"bool"
   shows "x [+] False = x"
   using xor_def by auto
 
-named_theorems rewrite_bool_xor_true \<open>automatically_generated\<close>
+named_theorems bool_xor_true \<open>automatically_generated\<close>
 
-lemma [rewrite_bool_xor_true]:
+lemma [bool_xor_true]:
   fixes x::"bool"
   shows "x [+] True = (\<not> x)"
   using xor_def by auto
 
-named_theorems rewrite_bool_xor_comm \<open>automatically_generated\<close>
+named_theorems bool_xor_comm \<open>automatically_generated\<close>
 
-lemma [rewrite_bool_xor_comm]:
+lemma [bool_xor_comm]:
   fixes x::"bool" and y::"bool"
   shows "x [+] y = y [+] x"
   using xor_def by auto
 
-named_theorems rewrite_bool_xor_elim \<open>automatically_generated\<close>
+named_theorems bool_xor_elim \<open>automatically_generated\<close>
 
-lemma [rewrite_bool_xor_elim]:
+lemma [bool_xor_elim]:
   fixes x::"bool" and y::"bool"
   shows "x [+] y = (x \<noteq> y)"
   using xor_def by auto
 
-named_theorems rewrite_ite_neg_branch \<open>automatically_generated\<close>
+named_theorems ite_neg_branch \<open>automatically_generated\<close>
 
-lemma [rewrite_ite_neg_branch]:
+lemma [ite_neg_branch]:
   fixes c::"bool" and x::"bool" and y::"bool"
   shows "(\<not> y) = x \<longrightarrow> (if c then x else y) = (c = x)"
   by auto
 
-named_theorems rewrite_ite_then_true \<open>automatically_generated\<close>
+named_theorems ite_then_true \<open>automatically_generated\<close>
 
-lemma [rewrite_ite_then_true]:
+lemma [ite_then_true]:
   fixes c::"bool" and x::"bool"
   shows "(if c then True else x) = (c \<or> x)"
   by auto
 
-named_theorems rewrite_ite_else_false \<open>automatically_generated\<close>
+named_theorems ite_else_false \<open>automatically_generated\<close>
 
-lemma [rewrite_ite_else_false]:
+lemma [ite_else_false]:
   fixes c::"bool" and x::"bool"
   shows "(if c then x else False) = (c \<and> x)"
   by auto
 
-named_theorems rewrite_ite_then_false \<open>automatically_generated\<close>
+named_theorems ite_then_false \<open>automatically_generated\<close>
 
-lemma [rewrite_ite_then_false]:
+lemma [ite_then_false]:
   fixes c::"bool" and x::"bool"
   shows "(if c then False else x) = (\<not> c \<and> x)"
   by auto
 
-named_theorems rewrite_ite_else_true \<open>automatically_generated\<close>
+named_theorems ite_else_true \<open>automatically_generated\<close>
 
-lemma [rewrite_ite_else_true]:
+lemma [ite_else_true]:
   fixes c::"bool" and x::"bool"
   shows "(if c then x else True) = (\<not> c \<or> x)"
   by auto
 
-named_theorems rewrite_ite_then_lookahead_self \<open>automatically_generated\<close>
+named_theorems ite_then_lookahead_self \<open>automatically_generated\<close>
 
-lemma [rewrite_ite_then_lookahead_self]:
+lemma [ite_then_lookahead_self]:
   fixes c::"bool" and x::"bool"
   shows "(if c then c else x) = (if c then True else x)"
   by auto
 
-named_theorems rewrite_ite_else_lookahead_self \<open>automatically_generated\<close>
+named_theorems ite_else_lookahead_self \<open>automatically_generated\<close>
 
-lemma [rewrite_ite_else_lookahead_self]:
+lemma [ite_else_lookahead_self]:
   fixes c::"bool" and x::"bool"
   shows "(if c then x else c) = (if c then x else False)"
   by auto
