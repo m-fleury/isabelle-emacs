c82fcdf7621dd591e8ac254158c059fc45c160ae
Hanna Lachnitt <lachnitt@stanford.edu>
2023-05-05 22:58:09 -0700

    New set rewrites

--- src/HOL/Tools/SMT/cvc5_dsl_rewrites/Set_Rewrites.thy
+++ src/HOL/Tools/SMT/cvc5_dsl_rewrites/Set_Rewrites.thy
@@ -15,6 +15,7 @@ re_concat,
 str_concat,
 *)
 
+
 named_theorems rewrite_sets_member_singleton \<open>automatically_generated\<close>
 
 lemma [rewrite_sets_member_singleton]:
@@ -41,6 +42,27 @@ named_theorems rewrite_sets_inter_comm \<open>automatically_generated\<close>
 lemma [rewrite_sets_inter_comm]:
   fixes x::"'a::type set" and y::"'a::type set"
   shows "x \<inter> y = y \<inter> x"
+  by auto
+
+named_theorems rewrite_sets_inter_member \<open>automatically_generated\<close>
+
+lemma [rewrite_sets_inter_member]:
+  fixes x::"'a::type" and y::"'a::type set" and z::"'a::type set"
+  shows "(x \<in> y \<inter> z) = (x \<in> y \<and> x \<in> z)"
+  by auto
+
+named_theorems rewrite_sets_minus_member \<open>automatically_generated\<close>
+
+lemma [rewrite_sets_minus_member]:
+  fixes x::"'a::type" and y::"'a::type set" and z::"'a::type set"
+  shows "(x \<in> y - z) = (x \<in> y \<and> x \<notin> z)"
+  by auto
+
+named_theorems rewrite_sets_union_member \<open>automatically_generated\<close>
+
+lemma [rewrite_sets_union_member]:
+  fixes x::"'a::type" and y::"'a::type set" and z::"'a::type set"
+  shows "(x \<in> y \<union> z) = (x \<in> y \<or> x \<in> z)"
   by auto
 
 end
