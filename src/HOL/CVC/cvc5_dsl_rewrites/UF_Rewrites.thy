8ff50b737dfaf1f101779fead5ef44b9d0a10f0a
Hanna Lachnitt <lachnitt@stanford.edu>
2023-09-27 18:23:15 -0700

    fix rewrite

--- src/HOL/CVC/cvc5_dsl_rewrites/UF_Rewrites.thy
+++ src/HOL/CVC/cvc5_dsl_rewrites/UF_Rewrites.thy
@@ -11,7 +11,7 @@ lemma [rewrite_eq_symm]:
 named_theorems rewrite_eq_refl \<open>added by hand\<close>
 
 lemma [rewrite_eq_refl]:
-  shows "a = a"
+  shows "(a = a) = True"
   by auto
 
 named_theorems rewrite_distinct_binary_elim \<open>added by hand\<close>
