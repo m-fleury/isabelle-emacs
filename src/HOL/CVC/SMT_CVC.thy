bbab908e47348745d794098d5867130068e24681
Hanna Lachnitt <lachnitt@stanford.edu>
2025-01-02 21:57:20 +0100

    Re-activate rare rewrite reconstruction

--- src/HOL/CVC/SMT_CVC.thy
+++ src/HOL/CVC/SMT_CVC.thy
@@ -14,7 +14,7 @@ lemmas [arith_simp_cvc5] = Groups.monoid_mult_class.mult_1_right Nat.mult_Suc_ri
                      Nat.mult_0_right Nat.add_Suc_right Groups.monoid_add_class.add.right_neutral
                      Num.numeral_2_eq_2 Nat.One_nat_def Num.numeral_2_eq_2 Nat.One_nat_def
                      Nat.Suc_less_eq Nat.zero_less_Suc minus_nat.diff_0 Nat.diff_Suc_Suc Nat.le0
-
+                     smt_arith_simplify
 lemmas [cvc_evaluate] = arith_simp_cvc5
 
 
