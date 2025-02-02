d855b98baeb789916a16b57ec4f7098fa294575e
Hanna Lachnitt <lachnitt@stanford.edu>
2025-02-01 18:25:29 -0800

    add more real rewrites

--- src/HOL/CVC/cvc5_dsl_rewrites/Extra_Rewrites.thy
+++ src/HOL/CVC/cvc5_dsl_rewrites/Extra_Rewrites.thy
@@ -9,4 +9,32 @@ lemma [rewrite_arith_to_int_to_real]:
   shows "NO_MATCH cvc_a (undefined x) \<Longrightarrow> floor (of_int x ::real) = x"
   by simp
 
+named_theorems rewrite_arith_to_real_distrib_uminus \<open>\<close>
+
+lemma [rewrite_arith_to_real_distrib_uminus]:
+  fixes x::"int"
+  shows "NO_MATCH cvc_a (undefined x) \<Longrightarrow> -(of_int x ::real) = (of_int (-x) ::real)"
+  by simp
+
+named_theorems rewrite_arith_to_real_distrib_add \<open>\<close>
+
+lemma [rewrite_arith_to_real_distrib_add]:
+  fixes x::"int" and y::"int"
+  shows "NO_MATCH cvc_a (undefined (x,y)) \<Longrightarrow> (of_int (x + y) ::real) = (of_int x ::real) + (of_int y ::real)"
+  by simp
+
+named_theorems rewrite_arith_to_real_distrib_minus \<open>\<close>
+
+lemma [rewrite_arith_to_real_distrib_minus]:
+  fixes x::"int" and y::"int"
+  shows "NO_MATCH cvc_a (undefined (x,y)) \<Longrightarrow> (of_int (x - y) ::real) = (of_int x ::real) - (of_int y ::real)"
+  by simp
+
+named_theorems rewrite_arith_to_real_distrib_mult \<open>\<close>
+
+lemma [rewrite_arith_to_real_distrib_mult]:
+  fixes x::"int" and y::"int"
+  shows "NO_MATCH cvc_a (undefined (x,y)) \<Longrightarrow> (of_int (x * y) ::real) = (of_int x ::real) * (of_int y ::real)"
+  by simp
+
 end
