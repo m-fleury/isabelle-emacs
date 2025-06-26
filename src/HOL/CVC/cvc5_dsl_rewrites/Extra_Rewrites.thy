b7704686856afbe4e1c2b787fd8b0df1c5809e20
lachnitt <lachnitt@stanford.edu>
2025-06-26 17:17:49 +0200

    rename rewrite

--- src/HOL/CVC/cvc5_dsl_rewrites/Extra_Rewrites.thy
+++ src/HOL/CVC/cvc5_dsl_rewrites/Extra_Rewrites.thy
@@ -385,9 +385,9 @@ lemma [rewrite_arith_leq_ite_lift]:
  \<Longrightarrow> (((if C then t else s) < r) = (if C then (t < r) else (s < r)))"
   by auto
 
-named_theorems or_not_refl \<open>\<close>
+named_theorems rewrite_or_not_refl \<open>\<close>
 
-lemma [or_not_refl]:
+lemma [rewrite_or_not_refl]:
   fixes t::'a and F'::bool
   shows "NO_MATCH cvc_a (undefined t F') \<Longrightarrow>  
    (t \<noteq> t) \<or> F' = F'"
