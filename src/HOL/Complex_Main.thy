2b653d8ccbaed1e8ccf4f18a8cea895723566813
Mathias Fleury <fleury@cs.uni-freiburg.de>
2026-04-09 20:52:00 +0200

    fix hidden notations

--- src/HOL/Complex_Main.thy
+++ src/HOL/Complex_Main.thy
@@ -11,4 +11,16 @@ begin
 
 notation floor (\<open>(\<open>open_block notation=\<open>mixfix floor\<close>\<close>\<lfloor>_\<rfloor>)\<close>)
 
+(*repeated to be independant of the import order*)
+no_notation
+  ordLeq2 (infix \<open><=o\<close> 50) and
+  ordLeq3 (infix \<open>\<le>o\<close> 50) and
+  ordLess2 (infix \<open><o\<close> 50) and
+  ordIso2 (infix \<open>=o\<close> 50) and
+  card_of (\<open>(\<open>open_block notation=\<open>mixfix card_of\<close>\<close>|_|)\<close>) and
+  BNF_Cardinal_Arithmetic.csum (infixr \<open>+c\<close> 65) and
+  BNF_Cardinal_Arithmetic.cprod (infixr \<open>*c\<close> 80) and
+  BNF_Cardinal_Arithmetic.cexp (infixr \<open>^c\<close> 90) and
+  BNF_Def.convol (\<open>(\<open>indent=1 notation=\<open>mixfix convol\<close>\<close>\<langle>_,/ _\<rangle>)\<close>)
+
 end
