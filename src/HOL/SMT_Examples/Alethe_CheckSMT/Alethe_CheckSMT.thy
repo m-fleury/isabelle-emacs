0b5877b88f73c5c87c6be870ddcbece269b03590
pw194 <pascal.walter@email.uni-freiburg.de>
2026-04-15 12:32:03 +0200

    Add and_pos and or_neg smt_check example

--- src/HOL/SMT_Examples/Alethe_CheckSMT/Alethe_CheckSMT.thy
+++ src/HOL/SMT_Examples/Alethe_CheckSMT/Alethe_CheckSMT.thy
@@ -5,6 +5,8 @@ begin
 
 declare [[smt_trace=true,smt_verbose=true]]
 
-check_smt "~~/src/HOL/SMT_Examples/Alethe_CheckSMT/connective_def_xor.smt2" "~~/src/HOL/SMT_Examples/Alethe_CheckSMT/connective_def_xor.alethe"
-
+check_smt "~~/src/HOL/SMT_Examples/Alethe_CheckSMT/connective_def_xor_1.smt2" "~~/src/HOL/SMT_Examples/Alethe_CheckSMT/connective_def_xor_1.alethe"
+check_smt "~~/src/HOL/SMT_Examples/Alethe_CheckSMT/and_pos_1.smt2" "~~/src/HOL/SMT_Examples/Alethe_CheckSMT/and_pos_1.alethe"
+check_smt "~~/src/HOL/SMT_Examples/Alethe_CheckSMT/or_neg_1.smt2" "~~/src/HOL/SMT_Examples/Alethe_CheckSMT/or_neg_1.alethe"
+ 
 end
