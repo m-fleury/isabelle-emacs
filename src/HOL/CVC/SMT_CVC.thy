fca4dc99efedcf6f254b01d8221690c4118acf4a
Mathias Fleury <fleury@cs.uni-freiburg.de>
2026-04-16 13:47:16 +0200

    add check_smt_slice for slices not producing false

--- src/HOL/CVC/SMT_CVC.thy
+++ src/HOL/CVC/SMT_CVC.thy
@@ -1,6 +1,6 @@
 theory SMT_CVC \<comment> \<open>More Setup for CVC that should be in HOL eventually\<close>
   imports HOL.SMT "cvc5_dsl_rewrites/Rare_Interface"
-  keywords "smt_status" "check_smt_dir" "check_smt" :: diag
+  keywords "smt_status" "check_smt_dir" "check_smt" "check_smt_slice" :: diag
 begin
 
 
@@ -129,7 +129,23 @@ val _ = Outer_Syntax.local_theory \<^command_keyword>\<open>check_smt_dir\<close
   in
    lthy
    end))
-
+val _ = Outer_Syntax.local_theory \<^command_keyword>\<open>check_smt_slice\<close>
+          "parse a file in SMTLIB2 format and check proof. <problem_file,proof_file>"
+    (Scan.optional (\<^keyword>\<open>(\<close> |-- Parse.string --| \<^keyword>\<open>)\<close>) "cvc5" --
+    (Parse.string -- Parse.string)
+    >> (fn (prover, (problem_file_name,proof_file_name)) => fn lthy =>
+  let
+    val ctxt = Local_Theory.target_of lthy
+    fun pretty tag lines = map Pretty.str lines |> Pretty.big_list tag |> Pretty.string_of
+    val _ = SMT_Config.verbose_msg ctxt (pretty "Checking Alethe proof...") []
+    (*Replay proof*)
+    val lthy' = Local_Theory.map_contexts (K (Config.put SMT_Config.slice_only_no_full_proof_attr true)) lthy
+    val _ = SMT_Check_External.check_smt (if prover = "cvc5" then "cvc5_proof" else prover)
+        problem_file_name proof_file_name NONE lthy'
+    val _ = SMT_Config.verbose_msg ctxt (pretty "Finished checking Alethe proof!") []
+  in
+   lthy
+  end))
 \<close>
 
 
