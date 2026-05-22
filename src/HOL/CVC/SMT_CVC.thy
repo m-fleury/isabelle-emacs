604988b116a632c218173cfa0be72b8076f6c56d
Mathias Fleury <fleury@cs.uni-freiburg.de>
2026-05-22 13:31:31 +0200

    closer to devel version

--- src/HOL/CVC/SMT_CVC.thy
+++ src/HOL/CVC/SMT_CVC.thy
@@ -1,11 +1,7 @@
 theory SMT_CVC \<comment> \<open>More Setup for CVC that should be in HOL eventually\<close>
   imports HOL.SMT "HOL.Alethe_Rare_Interface"
-  keywords "smt_status" "check_smt_dir" "check_smt" "check_smt_slice" :: diag
+  keywords "smt_status" :: diag
 begin
-
-(*lemmas [cvc_evaluate] = arith_simp_cvc5*)
-
-
 
 (*Term rewrites*)
 ML \<open>
@@ -51,62 +47,5 @@ if is_none (SMT_Builtin.dest_builtin_fun @{context}
   ("int.pow2", @{typ "int \<Rightarrow> int"})
    [@{term "2::int"}])
 then error "fail to recognize int.pow2" else ()\<close>
-
-(*External proof checking*)
-ML_file \<open>ML/smt_parse_problem.ML\<close>
-ML_file \<open>ML/smt_check_external.ML\<close>
-
-
-
-(*Das soll in AFP entry*)
-ML \<open>
-
-(*Call replay from SMT_Solver and add replay_data on your own*)
-val _ = Outer_Syntax.local_theory \<^command_keyword>\<open>check_smt\<close>
-          "parse a file in SMTLIB2 format and check proof. <problem_file,proof_file>"
-    (Scan.optional (\<^keyword>\<open>(\<close> |-- Parse.string --| \<^keyword>\<open>)\<close>) "cvc5" --
-    (Parse.string -- Parse.string)
-    >> (fn (prover, (problem_file_name,proof_file_name)) => fn lthy =>
-  let
-    val ctxt = Local_Theory.target_of lthy
-    fun pretty tag lines = map Pretty.str lines |> Pretty.big_list tag |> Pretty.string_of
-    val _ = SMT_Config.verbose_msg ctxt (pretty "Checking Alethe proof...") []
-    (*Replay proof*)
-    val _ = SMT_Check_External.check_smt (if prover = "cvc5" then "cvc5_proof" else prover)
-        problem_file_name proof_file_name NONE lthy
-    val _ = SMT_Config.verbose_msg ctxt (pretty "Finished checking Alethe proof!") []
-  in
-   lthy
-  end))
-
-(*Call replay from SMT_Solver and add replay_data on your own*)
-(*The problem (name.smt2) and proof files (name.alethe) should be in the same directory.*)
-val _ = Outer_Syntax.local_theory \<^command_keyword>\<open>check_smt_dir\<close>
-         "parse a directory with SMTLIB2 format and check proof. <dir>"
-    ((Scan.optional (\<^keyword>\<open>(\<close> |-- Parse.string --| \<^keyword>\<open>)\<close>) "cvc5" -- Parse.string)
-    >> (fn (prover, dir_name) => fn lthy =>
-  let
-    val _ = SMT_Check_External.check_all_benchmarks prover dir_name NONE lthy
-  in
-   lthy
-   end))
-val _ = Outer_Syntax.local_theory \<^command_keyword>\<open>check_smt_slice\<close>
-          "parse a file in SMTLIB2 format and check proof. <problem_file,proof_file>"
-    (Scan.optional (\<^keyword>\<open>(\<close> |-- Parse.string --| \<^keyword>\<open>)\<close>) "cvc5" --
-    (Parse.string -- Parse.string)
-    >> (fn (prover, (problem_file_name,proof_file_name)) => fn lthy =>
-  let
-    val ctxt = Local_Theory.target_of lthy
-    fun pretty tag lines = map Pretty.str lines |> Pretty.big_list tag |> Pretty.string_of
-    val _ = SMT_Config.verbose_msg ctxt (pretty "Checking Alethe proof...") []
-    (*Replay proof*)
-    val lthy' = Local_Theory.map_contexts (K (Config.put SMT_Config.slice_only_no_full_proof_attr true)) lthy
-    val _ = SMT_Check_External.check_smt (if prover = "cvc5" then "cvc5_proof" else prover)
-        problem_file_name proof_file_name NONE lthy'
-    val _ = SMT_Config.verbose_msg ctxt (pretty "Finished checking Alethe proof!") []
-  in
-   lthy
-  end))
-\<close>
 
 end
